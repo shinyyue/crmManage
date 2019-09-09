a61cb7b30d7d41b9099b44546fdc5df25da77806
shinyyue <763839213@qq.com>
2019-09-09 16:55:09 +0800

    crm 130地址

--- src/main.js
+++ src/main.js
@@ -109,7 +109,7 @@ Vue.component(Layout.name, Layout)
 Vue.component(TableList.name, TableList)
 
 Vue.prototype.API_ROOT = (process.env.NODE_ENV === 'production') ?
-    'http://47.105.130.130:8080/' : 'http://47.105.130.130:8080/'
+    'http://47.105.130.130:8100/' : 'http://47.105.130.130:8100/'
 
 // 路由拦截
 router.beforeEach((to, from, next) => {
